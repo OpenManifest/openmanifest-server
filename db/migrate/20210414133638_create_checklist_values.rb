32916c42f2c048c157a6ec31fc72e01b55f67ba4
Victor Rudolfsson <>
2021-04-21 22:47:15 +1000

    fix: Added Procfile

--- db/migrate/20210414133638_create_checklist_values.rb
+++ db/migrate/20210414133638_create_checklist_values.rb
@@ -4,8 +4,8 @@ class CreateChecklistValues < ActiveRecord::Migration[6.1]
   def change
     create_table :checklist_values do |t|
       t.references :checklist_item, null: false, foreign_key: true
-      t.references :created_by, null: false, foreign_key: { to_table: :dropzone_user }
-      t.references :updated_by, null: false, foreign_key: { to_table: :dropzone_user }
+      t.references :created_by, null: false, foreign_key: { to_table: :dropzone_users }
+      t.references :updated_by, null: false, foreign_key: { to_table: :dropzone_users }
       t.text :value, null: false
       t.timestamps
     end
