b74763119ec9c28ef63c0eaebb43a8f2aab27c6a
Victor Rudolfsson <>
2021-04-21 23:10:54 +1000

    fix: Typename typo

--- app/graphql/types/rig_inspection_type.rb
+++ app/graphql/types/rig_inspection_type.rb
@@ -4,8 +4,8 @@ module Types
   class RigInspectionType < Types::BaseObject
     field :id, GraphQL::Types::ID, null: false
     field :dropzone_user, Types::DropzoneUserType, null: false
-    field :inspected_by, Types::User, null: false
-    field :checklist, Types::Checklist, null: false
+    field :inspected_by, Types::UserType, null: false
+    field :checklist, Types::ChecklistType, null: false
     field :created_at, Int, null: false
     field :updated_at, Int, null: false
   end
