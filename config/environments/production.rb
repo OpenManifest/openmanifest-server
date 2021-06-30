fc67e9e2e9eee9ff37925b2cc3015415df8ab0dc
Victor Rudolfsson <>
2021-06-30 15:14:43 +1000

    fix: Add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,17 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_mailer.default_url_options = {:host => 'openmanifest.org', :protocol => 'http'}
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => Rails.application.credentials.dig(:sendgrid, :user_name),
+    :password => Rails.application.credentials.dig(:sendgrid, :password),
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options ={:host => 'openmanifest.org', :protocol => 'https'}
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
