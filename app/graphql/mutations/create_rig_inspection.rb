d3e390fcdd16ac6c017eb75a8e36c19c1f8078c2
Victor Rudolfsson <>
2021-07-04 12:43:53 +1000

    Fix association on rig inspection mutation

--- app/graphql/mutations/create_rig_inspection.rb
+++ app/graphql/mutations/create_rig_inspection.rb
@@ -19,7 +19,7 @@ module Mutations
       model.assign_attributes(attributes.to_h.except(:dropzone_id))
 
       model.form_template = dropzone.rig_inspection_template
-      model.inspected_by = context[:current_resource]
+      model.inspected_by = context[:current_resource].dropzone_users.find_by(dropzone: dropzone)
 
       model.save!
 
