d8bf72971223d6d719c2fd52d920734c618336a6
Victor Rudolfsson <>
2021-07-26 23:56:49 +1000

    fix: Timezones for users and dropzones

---
+++ db/migrate/20210726130407_add_time_zone_to_dropzones.rb
@@ -0,0 +1,5 @@
+class AddTimeZoneToDropzones < ActiveRecord::Migration[6.1]
+  def change
+    add_column :dropzones, :time_zone, :string, default: 'Australia/Brisbane'
+  end
+end
---
+++ db/migrate/20210726131353_add_load_number_to_load.rb
@@ -0,0 +1,5 @@
+class AddLoadNumberToLoad < ActiveRecord::Migration[6.1]
+  def change
+    add_column :loads, :load_number, :integer
+  end
+end
