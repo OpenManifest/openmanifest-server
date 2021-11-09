de0e9b976124c77a78785a880160653ad4e8180c
Victor Rudolfsson <>
2021-11-09 21:52:01 +1000

    fix: Linting

--- db/migrate/20211026101229_add_moderation_role_to_users.rb
+++ db/migrate/20211026101229_add_moderation_role_to_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddModerationRoleToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :moderation_role, :integer, default: 0
--- db/migrate/20211026102005_add_request_publication_to_dropzones.rb
+++ db/migrate/20211026102005_add_request_publication_to_dropzones.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRequestPublicationToDropzones < ActiveRecord::Migration[6.1]
   def change
     add_column :dropzones, :request_publication, :boolean, default: false
