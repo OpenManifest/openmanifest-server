119348cef41f4034fbc328c25a0de0695481e78f
Victor Rudolfsson <>
2021-11-13 13:09:40 +1000

    fix: Email sender URL matching SPF record

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
     address: ENV["MAILGUN_SMTP_SERVER"],
     user_name: ENV["MAILGUN_SMTP_LOGIN"],
     password: ENV["MAILGUN_SMTP_PASSWORD"],
-    domain: "openmanifest.org",
+    domain: "mail.openmanifest.org",
     authentication: :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
