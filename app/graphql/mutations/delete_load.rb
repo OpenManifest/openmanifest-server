4d5faa56ec26c7321e3938b763fe75a232ca68bb
amnethesia <flowsnake@icloud.com>
2022-01-23 12:40:03 +1000

    fix: Mutation arguments

--- app/graphql/mutations/delete_load.rb
+++ app/graphql/mutations/delete_load.rb
@@ -3,6 +3,8 @@
 module Mutations
   class DeleteLoad < Mutations::BaseMutation
     field :load, Types::LoadType, null: true
+    field :errors, [String], null: true
+    field :field_errors, [Types::FieldErrorType], null: true
 
     argument :id, Int, required: true
 
