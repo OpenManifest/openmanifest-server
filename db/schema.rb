33e95de732f51ae113c6c150db5a00e786cef08a
amnethesia <flowsnake@icloud.com>
2022-02-01 21:47:35 +1000

    feat: Log in with Facebook

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_122109) do
+ActiveRecord::Schema.define(version: 2022_02_01_105426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 2022_01_18_122109) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
+  end
+
+  create_table "authentication_providers", force: :cascade do |t|
+    t.string "uid"
+    t.integer "provider"
+    t.bigint "user_id", null: false
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_authentication_providers_on_user_id"
   end
 
   create_table "dropzone_users", force: :cascade do |t|
@@ -510,6 +520,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_122109) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "authentication_providers", "users"
   add_foreign_key "dropzone_users", "dropzones"
   add_foreign_key "dropzone_users", "licenses"
   add_foreign_key "dropzone_users", "user_roles"
