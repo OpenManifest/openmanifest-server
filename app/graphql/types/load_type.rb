e4865ffb270d732479b0f84d8ae39136228ca4e8
amnethesia <flowsnake@icloud.com>
2022-02-26 18:53:52 +1000

    hotfix: Exit weight nil

--- app/graphql/types/load_type.rb
+++ app/graphql/types/load_type.rb
@@ -12,7 +12,7 @@ module Types
     field :weight, Integer, null: false
     def weight
       pilot_weight = object.pilot.try(:user).try(:exit_weight) || 0
-      pilot_weight + object.slots.sum(&:exit_weight)
+      pilot_weight + object.slots.map(&:exit_weight).reject(&:blank?).reduce(:+)
     end
 
     field :created_at, Int, null: false
