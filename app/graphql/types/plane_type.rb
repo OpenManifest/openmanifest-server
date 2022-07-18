d99e491712b4beb8cf9d61df073c8ca11a30369f
amnethesia <flowsnake@icloud.com>
2022-07-18 21:51:38 +1000

    fix: Field nullability

--- app/graphql/types/plane_type.rb
+++ app/graphql/types/plane_type.rb
@@ -7,7 +7,10 @@ module Types
     field :id, GraphQL::Types::ID, null: false
     field :name, String, null: true
     field :min_slots, Int, null: true
-    field :max_slots, Int, null: true
+    field :max_slots, Int, null: false
+    def max_slots
+      object.max_slots || object.min_slots || 0
+    end
     field :hours, Int, null: true
     field :next_maintenance_hours, Int, null: true
     field :registration, String, null: true
