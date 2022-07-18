47fbcc13983f688fdb5a7d21f025d66d8858db03
amnethesia <flowsnake@icloud.com>
2022-07-18 21:34:26 +1000

    fix: Activity query

--- app/graphql/resolvers/activity.rb
+++ app/graphql/resolvers/activity.rb
@@ -4,7 +4,7 @@ class Resolvers::Activity < Resolvers::Base
   type Types::Events::EventType.connection_type, null: false
   description "Get all Activity Events for a dropzone (or all dropzones)"
 
-  argument :dropzone, Int, required: true,
+  argument :dropzone, Int, required: false,
            description: "Filter by Dropzone",
            prepare: -> (value, ctx) { Dropzone.find(value) }
   argument :levels,  [Types::Events::EventLevelType], required: false
@@ -13,19 +13,19 @@ class Resolvers::Activity < Resolvers::Base
            description: "Filter by who created the event",
            prepare: -> (value, ctx) { DropzoneUser.find(value) }
   def resolve(
-    dropzone_id: nil,
+    dropzone: nil,
     created_by: nil,
     levels: nil,
     actions: nil,
     lookahead: nil
   )
     lookahead = lookahead.selection(:edges).selection(:node)
-    query = ::Activity::Event
-    query = query.includes(:created_by) if lookahead.selects?(:created_by)
-    query = query.where(dropzone_id: dropzone_id)           if dropzone_id
+    query = dropzone.events                                 if dropzone
+    query ||= ::Activity::Event
+    query = query.includes(:created_by)                     if lookahead.selects?(:created_by)
     query = query.where(level: level)                       if level
     query = query.where.not(level: :debug)                  unless level
-    query = query.where(dropzone_user_id: dropzone_user_id) if dropzone_user_id
+    query = query.where(created_by: dropzone_user_id)       if created_by
     query.order(created_at: :desc)
   end
 end
