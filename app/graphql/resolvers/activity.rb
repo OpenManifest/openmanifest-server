dbd4f86d1c90cba96562fb4aa7475cf0e351eab0
Amnesthesia <flowsnake@icloud.com>
2022-07-18 21:39:46 +1000

    Bug/activity query2 (#28)
    * fix: Activity query

--- app/graphql/resolvers/activity.rb
+++ app/graphql/resolvers/activity.rb
@@ -4,14 +4,15 @@ class Resolvers::Activity < Resolvers::Base
   type Types::Events::EventType.connection_type, null: false
   description "Get all Activity Events for a dropzone (or all dropzones)"
 
-  argument :dropzone, Int, required: false,
+
+  argument :dropzone, [Int], required: true,
            description: "Filter by Dropzone",
-           prepare: -> (value, ctx) { Dropzone.find(value) }
+           prepare: -> (value, ctx) { Dropzone.where(value) }
   argument :levels,  [Types::Events::EventLevelType], required: false
   argument :actions, [Types::Events::EventActionType], required: false
-  argument :created_by, Int, required: false,
+  argument :created_by, [Int], required: false,
            description: "Filter by who created the event",
-           prepare: -> (value, ctx) { DropzoneUser.find(value) }
+           prepare: -> (value, ctx) { DropzoneUser.where(value) }
   def resolve(
     dropzone: nil,
     created_by: nil,
@@ -20,12 +21,13 @@ class Resolvers::Activity < Resolvers::Base
     lookahead: nil
   )
     lookahead = lookahead.selection(:edges).selection(:node)
-    query = dropzone.events                                 if dropzone
-    query ||= ::Activity::Event
-    query = query.includes(:created_by)                     if lookahead.selects?(:created_by)
+
+    query = ::Activity::Event
+    query = query.includes(:created_by) if lookahead.selects?(:created_by)
+    query = query.where(dropzone: dropzone)                 if dropzone
     query = query.where(level: level)                       if level
     query = query.where.not(level: :debug)                  unless level
-    query = query.where(created_by: dropzone_user_id)       if created_by
+    query = query.where(created_by: created_by)             if created_by
     query.order(created_at: :desc)
   end
 end
