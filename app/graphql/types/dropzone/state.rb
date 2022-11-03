0191160243225f94f0f6a203f7ecaab704a095e4
Amnesthesia <flowsnake@icloud.com>
2022-11-03 23:05:44 +1000

    fix: Dropzone state missing in type (#57)
    Co-authored-by: Victor Rudolfsson <>

--- app/graphql/types/dropzone/state.rb
+++ app/graphql/types/dropzone/state.rb
@@ -2,7 +2,7 @@
 
 class Types::Dropzone::State < Types::BaseEnum
   graphql_name "DropzoneState"
-  Dropzone.state_machine.states.keys.each do |state|
+  ::Dropzone.state_machine.states.keys.each do |state|
     value state
   end
 end
