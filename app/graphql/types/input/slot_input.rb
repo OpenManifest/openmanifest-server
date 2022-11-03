96fabcde04b15fcaca561c9e3e74ecb54b5be6af
Victor Rudolfsson <>
2022-11-03 22:30:58 +1000

    fix: Fixed scope in Ghost inputtype

--- app/graphql/types/input/slot_input.rb
+++ app/graphql/types/input/slot_input.rb
@@ -3,19 +3,19 @@
 module Types
   module Input
     class SlotInput < Types::BaseInputObject
-      argument :dropzone_user, Int, required: false,
-               prepare: -> (value, ctx) { DropzoneUser.find_by(id: value) }
-      argument :ticket_type, Int, required: false,
-               prepare: -> (value, ctx) { TicketType.find_by(id: value) }
-      argument :jump_type, Int, required: false,
-               prepare: -> (value, ctx) { JumpType.find_by(id: value) }
-      argument :load, Int, required: false,
-               prepare: -> (value, ctx) { Load.find_by(id: value) }
-      argument :rig, Int, required: false,
-               prepare: -> (value, ctx) { Rig.find_by(id: value) }
+      argument :dropzone_user, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::DropzoneUser.find_by(id: value) }
+      argument :ticket_type, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::TicketType.find_by(id: value) }
+      argument :jump_type, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::JumpType.find_by(id: value) }
+      argument :load, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::Load.find_by(id: value) }
+      argument :rig, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::Rig.find_by(id: value) }
       argument :exit_weight, Float, required: false
-      argument :extras, [Int], required: false,
-               prepare: -> (value, ctx) { Extra.where(id: value) }
+      argument :extras, [GraphQL::Types::ID], required: false,
+               prepare: -> (value, ctx) { ::Extra.where(id: value) }
 
       argument :group_number, Int, required: false
       argument :user_group, [Types::Input::SlotUser], required: false
--- app/graphql/types/input/slot_user.rb
+++ app/graphql/types/input/slot_user.rb
@@ -5,8 +5,8 @@ module Types
     class SlotUser < Types::BaseInputObject
       argument :id, Int, required: true
       argument :exit_weight, Float, required: true
-      argument :rig, Int, required: false,
-               prepare: -> (value, ctx) { Rig.find_by(id: value) }
+      argument :rig, GraphQL::Types::ID, required: false,
+               prepare: -> (value, ctx) { ::Rig.find_by(id: value) }
       argument :passenger_name, String, required: false
       argument :passenger_exit_weight, Float, required: false
     end
