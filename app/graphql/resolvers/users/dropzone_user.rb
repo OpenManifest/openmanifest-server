c0b715a75034c220f0c2658bf51cb72ea15fed3b
Amnesthesia <flowsnake@icloud.com>
2022-11-04 23:31:16 +1000

    fix: Dropzone User resolvers (#59)
    Co-authored-by: Victor Rudolfsson <>

---
+++ app/graphql/resolvers/users/dropzone_user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Resolvers::Users::DropzoneUser < Resolvers::Base
+  type Types::DropzoneUserType.connection_type, null: true
+  description "Get a specific user at a dropzone"
+
+  argument :id, ID, required: true
+
+  def resolve(id: nil, lookahead: nil)
+    return nil unless id
+    query = DropzoneUser
+    query = query.includes(:dropzone)   if lookahead.selects?(:dropzone)
+    query = query.includes(:jump_types) if lookahead.selects?(:jump_type)
+    query = query.includes(:slots)      if lookahead.selects?(:slots)
+    query = query.includes(:user)       if lookahead.selects?(:user)
+    query = query.includes(:license)    if lookahead.selects?(:license)
+    query.find_by(id: id)
+  end
+end
