7dfb299ecef36a8b7c0da2cb7b4b0852c3af39da
Victor Rudolfsson <>
2022-11-08 17:52:20 +1000

    fix: Added validation to loads

--- app/graphql/types/any_resource_type.rb
+++ app/graphql/types/any_resource_type.rb
@@ -4,9 +4,12 @@ module Types
   module AnyResourceType
     include Types::BaseInterface
     field :id, ID, null: false
-    field :guid, ID, null: false
-    def guid
-      object.to_gid_param
+
+    definition_methods do
+      # Determine what object type to use for `object`
+      def resolve_type(object, context)
+        ::DzSchema.resolve_type(object.class, object, context)
+      end
     end
   end
 end
