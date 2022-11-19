0fc90a1c6209dddd3fc33deb8a9659cd8694fbcf
Victor Rudolfsson <>
2022-11-19 14:51:18 +1000

    fix: Create Ghost

--- app/graphql/resolvers/users/dropzone_users.rb
+++ app/graphql/resolvers/users/dropzone_users.rb
@@ -38,7 +38,7 @@ class Resolvers::Users::DropzoneUsers < Resolvers::Base
         )
       )
     end
-    query = query.search(search) if search.present?
+    query = query.search(search) unless search.blank?
     query
   end
 end
