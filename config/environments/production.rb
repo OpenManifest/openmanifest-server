53c5756098379db947e0babf76a78021b135e86c
Amnesthesia <flowsnake@icloud.com>
2023-01-16 22:20:38 +1000

    Use redis adapter for actioncable (#113)
    Co-authored-by: Victor Rudolfsson <>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0") }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
